10056c29d34cff169e0f3a9cf742f066b4686a10
Shaun Robinson <connecshaun@gmail.com>
2021-04-08 18:29:20 +0000

    working with functions

---
+++ hello.js
@@ -0,0 +1,22 @@
+/* const sayHello = function () {
+
+  console.log('Hello, world');
+}
+
+sayHello(); */
+
+/*const sayHello = function (name) {
+  console.log('Hello, ' + name);
+}
+
+sayHello('Brody');
+sayHello('Shaun');
+sayHello('Mark'); */
+
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
+
